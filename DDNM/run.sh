768dedd9669e8a9c5f1e1fb835c46ca2de053e04
Clover698 <s10310698@gmail.com>
2024-12-09 11:23:24 +0800

    DDNM_v1

--- DDNM/run.sh
+++ DDNM/run.sh
@@ -1,4 +1,4 @@
-# Please modify the dataset path of imagenet in datasets/__init__.py (line 176). You can download the dataset from http://image-net.org/download-images. Note the dataset is large up to 200GB.
+# Please modify the dataset path of imagenet in datasets/__init__.py (line 176 & 180). You can download the dataset from http://image-net.org/download-images. Note the dataset is large up to 200GB.
 
 # Train subtask 1
 python train.py --ni --config imagenet_256.yml --path_y imagenet --eta 0.85 --deg "sr_bicubic" --deg_scale 4 --sigma_y 0. -i imagenet_sr_bc_4_5 --target_steps 5
