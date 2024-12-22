4855961cd5e6b61368639495ed760a73c4a7cf90
qiannnhui <b09901160@ntu.edu.tw>
2024-12-23 03:26:01 +0800

    fix:bash

--- DDNM/run.sh
+++ DDNM/run.sh
@@ -3,28 +3,26 @@
 # Define common arguments
 CONFIG="celeba_hq.yml"
 DOC="celeba"
-TIMESTEPS=100
 ETA=0.85
-ETAB=1
 SIGMA_0=0.
 INPUT="celeba_hq_sr4_sigma_0.05"
-INPUT_ROOT="./"
+INPUT_ROOT="/disk_195a/qiannnhui"
 
 # Training and evaluation for "sr4"
 for DEG in sr_bicubic deblur_uni
 do
     echo "Starting training and evaluation for ${DEG}..."
     for TARGET_STEPS in 5 10 20; do
-        python train.py --ni --config $CONFIG --doc $DOC --timesteps $TIMESTEPS \
-            --eta $ETA --etaB $ETAB --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
+        python train.py --ni --config $CONFIG --path_y $DOC \
+            --eta $ETA --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
             -i $INPUT --target_steps $TARGET_STEPS --input_root $INPUT_ROOT
 
-        python train.py --ni --config $CONFIG --doc $DOC --timesteps $TIMESTEPS \
-            --eta $ETA --etaB $ETAB --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
+        python train.py --ni --config $CONFIG --path_y $DOC \
+            --eta $ETA --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
             -i $INPUT --second_stage --target_steps $TARGET_STEPS --input_root $INPUT_ROOT
 
-        python eval.py --ni --config $CONFIG --doc $DOC --timesteps $TIMESTEPS \
-            --eta $ETA --etaB $ETAB --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
+        python eval.py --ni --config $CONFIG --path_y $DOC \
+            --eta $ETA --deg $DEG --sigma_y $SIGMA_0 --deg_scale 4 \
             -i $INPUT --target_steps $TARGET_STEPS --eval_model_name ${DEG}_2agent_A2C_${TARGET_STEPS} --input_root $INPUT_ROOT
     done
 
